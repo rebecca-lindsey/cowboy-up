c3ad5ab076d14d7ff7b0ea3487d76e3965f217f0
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-12-10 15:16:23 -0500

    Add colored horse icons

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,3 +1,31 @@
 class Horse < ActiveRecord::Base
     belongs_to :user
+
+    def self.color_selector(color)
+        case color
+        when "Bay", "Bay Dapple"
+            "bay"
+        when "Appaloosa", "White"
+            "white"
+        when "Black"
+            "black"
+        when "Brown"
+            "brown"
+        when "Buckskin", "Palomino", "Dun"
+            "gold"
+        when "Champagne"
+            "champagne"
+        when "Chestnut", "Flaxen Chestnut"
+            "chestnut"
+        when "Cream"
+            "cream"
+        when "Gray", "Silver Dapple", "Grulla"
+            "gray"
+        when "Roan"
+            "roan"
+        end
+    end
 end
+
+        # <option value = "Grulla">Grulla</option>
+        # <option value = "Roan">Roan</option>
