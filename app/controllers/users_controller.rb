36018a1808f0dc2b5e7aac1698acdafb2adcbc29
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-12-10 21:08:31 -0500

    Tidy up users_controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,48 +9,43 @@ class UsersController < ApplicationController
 
   post '/signup' do
     u = User.new(email: params["email"], name: params["name"].titleize, password: params["password"])
-    if u.email.blank? || u.name.blank? || u.password.blank? || User.find_by_email(params["email"]) || !URI::MailTo::EMAIL_REGEXP.match?(params["email"])
-            # Invalid signup
-           redirect '/signup'
-        else
-            # Valid signup
-            u.save
-            session[:user_id] = u.id
-            # Signs in user and sends them to their horses
-            redirect "/users/#{u.id}"
-        end
+    if invalid_signup?
+      redirect '/signup'
+    else
+      u.save
+      session[:user_id] = u.id
+      redirect "/users/#{u.id}"
+    end
   end
 
   get '/login' do
-    if logged_in?
-      redirect "/users/#{current_user.id}"
-    #   if they are logged in, take them to their horses
-    end
+    redirect "/users/#{current_user.id}" if logged_in?
     erb :'/users/login'
-    # Not logged in, show the login form
   end
 
   post '/login' do
     user = User.find_by_email(params[:email])
-        if user && user.authenticate(params[:password])
-            session[:user_id] = user.id
-            redirect "/users/#{current_user.id}"
-        else
-            # if invalid login, display error and redirect somewhere
-            redirect '/signup'
-        end
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/users/#{current_user.id}"
+    else
+      redirect '/login'
+    end
   end
 
     get '/logout' do
-        if logged_in?
-            session.destroy
-        end
-            redirect to '/'
+      session.destroy if logged_in?
+      redirect to '/'
     end
 
     get '/users/:id' do
-        @user = User.all.find{|user| user.id.to_s == params[:id]}
-        erb :'/users/show'
+      @user = User.all.find{|user| user.id.to_s == params[:id]}
+      erb :'/users/show'
     end
 
+
+    private
+    def invalid_signup?
+      true if u.email.blank? || u.name.blank? || u.password.blank? || User.find_by_email(params["email"]) || !URI::MailTo::EMAIL_REGEXP.match?(params["email"])
+    end
 end
