3888a59f47b4eb3cfeb8073c061fd116ec94f9c6
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-12-10 22:00:55 -0500

    Correct user delete action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
-    has_many :horses, dependent: :destroy
+    has_many :horses, dependent: :delete_all
 
     def self.all_alpha_sort
         all.order(:name)
