dbf5184e455efea4ec814961ce142e699fd8156f
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-12-10 21:13:23 -0500

    Clean up horses_controller

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -1,81 +1,69 @@
 class HorsesController < ApplicationController
-    get '/horses' do
-        redirect_if_not_logged_in
-        @users_alpha_sort = User.all_alpha_sort.includes(:horses)
-        erb :'/horses/index'
+  get '/horses' do
+    redirect_if_not_logged_in
+    @users_alpha_sort = User.all_alpha_sort.includes(:horses)
+    erb :'/horses/index'
+  end
+
+  get '/horses/new' do
+    redirect_if_not_logged_in
+    @colors = Horse.color_list
+    @sexes = Horse.sex_list
+    @breeds = Horse.breeds
+    erb :'/horses/new'
+  end
+
+  post '/horses' do
+    redirect_if_not_logged_in
+    redirect "/horses/new" if invalid_input? || blank_input? || invalid_name?
+    @horse = Horse.new(name: params[:name].titleize, sex: params[:sex], color: params[:color], breed: params[:breed].titleize)
+    current_user.horses << @horse
+    redirect "users/#{current_user.id}"
+  end
+
+  get '/horses/:id' do
+    redirect_if_not_logged_in
+    @horse = Horse.find_by_id(params[:id])
+    redirect_if_no_horse
+    erb :'/horses/show'
+  end
+
+  get '/horses/:id/edit' do
+    @horse = Horse.find_by_id(params[:id])
+    redirect_if_not_authorized
+    @colors = Horse.color_list
+    @breeds = Horse.breeds
+    @sexes = Horse.sex_list
+    erb :'/horses/edit'
+  end
+
+  patch '/horses/:id' do
+    @horse = Horse.find_by_id(params[:id])
+    redirect_if_not_authorized
+    redirect "/horses/#{params[:id]}/edit" if invalid_input? || blank_input? || invalid_name?
+    @horse.update(name: params[:name], sex: params[:sex], color: params[:color], breed: params[:breed])
+    redirect "/horses/#{@horse.id}"
     end
 
-    get '/horses/new' do
-        redirect_if_not_logged_in
-        @colors = Horse.color_list
-        @sexes = Horse.sex_list
-        @breeds = Horse.breeds
-        erb :'/horses/new'
-    end
-
-    post '/horses' do
-        if invalid_input? || blank_input? || invalid_name?
-            redirect "/horses/new"
-        end
-        @horse = Horse.new(name: params[:name].titleize, sex: params[:sex], color: params[:color], breed: params[:breed].titleize)
-        redirect_if_not_logged_in
-        current_user.horses << @horse
-        redirect "users/#{current_user.id}"
-    end
-
-    get '/horses/:id' do
-        redirect_if_not_logged_in
-        @horse = Horse.find_by_id(params[:id])
-        redirect_if_no_horse
-        erb :'/horses/show'
-    end
-
-    get '/horses/:id/edit' do
-        @horse = Horse.find_by_id(params[:id])
-        redirect_if_not_authorized
-        @colors = Horse.color_list
-        @breeds = Horse.breeds
-        @sexes = Horse.sex_list
-        erb :'/horses/edit'
-    end
-
-    patch '/horses/:id' do
-        @horse = Horse.find_by_id(params[:id])
-        redirect_if_not_authorized
-        if invalid_input? || blank_input? || invalid_name?
-            redirect "/horses/#{params[:id]}/edit"
-        else
-            @horse.update(name: params[:name], sex: params[:sex], color: params[:color], breed: params[:breed])
-            redirect "/horses/#{@horse.id}"
-        end
-    end
-
-    delete '/horses/:id' do
-        @horse = Horse.find_by_id(params[:id])
-        redirect_if_not_authorized
-        @horse.delete
-        redirect to "/users/#{current_user.id}"
-    end
+  delete '/horses/:id' do
+    @horse = Horse.find_by_id(params[:id])
+    redirect_if_not_authorized
+    @horse.delete
+    redirect to "/users/#{current_user.id}"
+  end
 
     private
   def redirect_if_not_logged_in
-    if !logged_in?
-      redirect '/login'
-    end
+    redirect '/login' if !logged_in?
   end
 
-    def redirect_if_not_authorized
-    if !logged_in?
-        redirect '/login'
-    elsif !@horse || current_user != @horse.user
-        redirect '/horses'
-    end
+  def redirect_if_not_authorized
+    redirect '/login' if !logged_in?
+    redirect '/horses' if !@horse || current_user != @horse.user
   end
 
   def redirect_if_no_horse
-    if !@horse
-        redirect "/horses"
-    end
+    redirect "/horses" if !@horse
   end
 
   def invalid_input?
